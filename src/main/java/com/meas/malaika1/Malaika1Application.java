5c78960eeac9aad0f5a659da7a30d06bc16c3b4b
itpromeas <itpromeas@gmail.com>
2023-07-11 19:25:35 +0200

    initial commit

---
+++ src/main/java/com/meas/malaika1/Malaika1Application.java
@@ -0,0 +1,14 @@
+package com.meas.malaika1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Malaika1Application {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(Malaika1Application.class, args);
+	}
+
+}
